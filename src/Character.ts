9847aae48128286b38f4d42a1dd12be71d8b20bd
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-08-22 17:26:27 -0300

    implementa atributos necessarios para Characters e também metodos receiveDamage/attack e falta implementar metodo level up e special

---
+++ src/Character.ts
@@ -0,0 +1,81 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter, { SimpleFighter } from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(name: string) {
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
+
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;  
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  get energy(): Energy {
+    const object = { ...this._energy };
+    return object;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damageReceived = attackPoints - this._defense;
+    if (damageReceived > 0) {
+      this._lifePoints -= damageReceived;
+    }
+    if (damageReceived <= 0) {
+      this._lifePoints -= 1;
+    }
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    console.log('Level up!');
+  }
+}
